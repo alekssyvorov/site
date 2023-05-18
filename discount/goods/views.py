ed7c779f4c693f6cfe85a6b0968c4447ad1efca7
Djekson <alekssyvorov29@gmail.com>
2023-05-18 23:45:53 +0300

    add products 3

--- discount/goods/views.py
+++ discount/goods/views.py
@@ -5,20 +5,18 @@ from .models import Register, Products
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h2>Привет мир</h2>")
+    return render(request, 'good/index.html')
 
 def register(request):
     return render(request, 'good/register.html')
 
 def addUser(request):
-    print(request)
     if request.POST:
         try:
             loginUser = request.POST.get("login")
             password = request.POST.get("password")
             email = request.POST.get("email")
             response = f'Привет {loginUser}{password}{email}'
-            print(response)
             dataBD = Register.objects.get_or_create(name=loginUser, password=password, email=email)
             # dataBD.save()
         except ValueError:
@@ -26,7 +24,7 @@ def addUser(request):
     else:
         response = 'не работает'
     return HttpResponse(response)
-    # return render(request, 'good/register.html')
+    # return render(response, 'good/register.html')
 def user_register(request):
     user_reg = Register.objects.all()
     return HttpResponse(user_reg)
